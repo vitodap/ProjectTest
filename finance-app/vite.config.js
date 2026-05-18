3e28361b6d9793ed42b119c0a43c0cab2a74d0cf
vitodap <72919165+vitodap@users.noreply.github.com>
2026-05-18 14:41:30 +0700

    Add files via upload

---
+++ finance-app/vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: './', // Membuat path aset menjadi relatif (Aman untuk GitHub Pages)
+})
